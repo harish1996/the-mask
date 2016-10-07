e0125447966c281eac134fc3c66600fe5f25045c
harish1996 <harishganesan96@gmail.com>
2016-10-07 13:23:24 +0530

    Added Menus

---
+++ js/main.js
@@ -0,0 +1,10 @@
+function theopen()
+{
+	document.getElementById("menu").style.width="250px";
+	document.getElementById("list").style.width="0px";
+}
+function theclose()
+{
+	document.getElementById("menu").style.width="0px";
+	document.getElementById("list").style.width="100%";
+}
